0a2b7492e056b86220c722506ba00027e5d60649
tobicg31 <82407605+tobicg31@users.noreply.github.com>
2025-10-09 13:20:47 -0300

    Add files via upload

--- JS/numeros.js
+++ JS/numeros.js
@@ -1,12 +1,28 @@
 let valueDisplays = document.querySelectorAll(".num");
-let interval = 2000;
+let interval = 5000;
 
 valueDisplays.forEach((valueDisplay) => {
     let startValue = 0;
     let endValue = parseInt(valueDisplay.getAttribute("data-val"));
     let duration = Math.floor(interval/endValue);
     let counter = setInterval(function(){
-        startValue+=1;
+        startValue+=2;
+        valueDisplay.textContent = startValue;
+        if (startValue == endValue){
+            clearInterval(counter);
+        }
+    }, duration);
+});
+
+let valueDisplayslg = document.querySelectorAll(".num-lg");
+let intervallg = 2000;
+
+valueDisplayslg.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval/endValue);
+    let counter = setInterval(function(){
+        startValue+=5;
         valueDisplay.textContent = startValue;
         if (startValue == endValue){
             clearInterval(counter);
